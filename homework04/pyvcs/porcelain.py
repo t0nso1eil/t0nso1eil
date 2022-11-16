830dbf9590e93ab7f7d47e6577ce3dfadb9e8c78
t0nso1eil <73792532+t0nso1eil@users.noreply.github.com>
2022-11-16 03:41:02 +0300

    Update porcelain.py

--- homework04/pyvcs/porcelain.py
+++ homework04/pyvcs/porcelain.py
@@ -1,5 +1,6 @@
 import os
 import pathlib
+import shutil
 import typing as tp
 
 from pyvcs.index import read_index, update_index
@@ -9,15 +10,34 @@ from pyvcs.tree import commit_tree, write_tree
 
 
 def add(gitdir: pathlib.Path, paths: tp.List[pathlib.Path]) -> None:
-    # PUT YOUR CODE HERE
-    ...
+    update_index(gitdir, paths)
 
 
 def commit(gitdir: pathlib.Path, message: str, author: tp.Optional[str] = None) -> str:
-    # PUT YOUR CODE HERE
-    ...
+    return commit_tree(gitdir=gitdir, tree=write_tree(gitdir, read_index(gitdir)), message=message, author=author)
 
 
 def checkout(gitdir: pathlib.Path, obj_name: str) -> None:
-    # PUT YOUR CODE HERE
-    ...
+    head = gitdir / "refs" / "heads" / obj_name
+    if head.exists():
+        with head.open("r") as f:
+            obj_name = f.read()
+    index = read_index(gitdir)
+    for i in index:
+        if pathlib.Path(i.name).is_file():
+            name = i.name.split("/")
+            if len(name) > 1:
+                shutil.rmtree(name[0])
+            else:
+                os.chmod(i.name, 0o777)
+                os.remove(i.name)
+    objfile = gitdir / "objects" / obj_name[:2] / obj_name[2:]
+    with objfile.open("rb") as f:
+        commit = f.read()
+    for i in find_tree_files(commit_parse(commit).decode(), gitdir):
+        name = i[0].split("/")
+        if len(name) > 1:
+            pathlib.Path(name[0]).absolute().mkdir()
+        with open(i[0], "w") as tree_path:
+            content = read_object(i[1], gitdir)[1]
+            tree_path.write(content.decode())
