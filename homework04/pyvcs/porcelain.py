c3793acb93de02699db58387338e3f17131d9991
t0nso1eil <73792532+t0nso1eil@users.noreply.github.com>
2022-11-16 14:41:09 +0300

    Update porcelain.py

--- homework04/pyvcs/porcelain.py
+++ homework04/pyvcs/porcelain.py
@@ -20,7 +20,6 @@ def commit(gitdir: pathlib.Path, message: str, author: tp.Optional[str] = None) 
 
 def checkout(gitdir: pathlib.Path, obname: str) -> None:
     head = gitdir / "refs" / "heads" / obname
-    
     if head.exists():
         with head.open("r") as f:
             obname = f.read()
