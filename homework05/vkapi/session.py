e867e718c01691ba4759a519faf2240ee20e8153
t0nso1eil <73792532+t0nso1eil@users.noreply.github.com>
2022-11-29 23:56:30 +0300

    ?????????????/

--- homework05/vkapi/session.py
+++ homework05/vkapi/session.py
@@ -44,7 +44,6 @@ class Session:
             except requests.exceptions.ReadTimeout:
                 raise requests.exceptions.ReadTimeout
         return response        
-
     def post(self, url: str, *args: tp.Any, **kwargs: tp.Any) -> requests.Response:
         count = 0
         for i in range(0, self.max_retries + 1):
