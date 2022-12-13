98c8980fb1ebd54eb0b9e9734e7ec362343dd500
t0nso1eil <73792532+t0nso1eil@users.noreply.github.com>
2022-12-13 18:16:58 +0300

    Update db.py

--- homework06/db.py
+++ homework06/db.py
@@ -1,13 +1,10 @@
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
 
 Base = declarative_base()
 engine = create_engine("sqlite:///news.db")
-session = sessionmaker(bind=engine)
-
+Session = sessionmaker(bind=engine)
 
 class News(Base):
     __tablename__ = "news"
@@ -18,5 +15,6 @@ class News(Base):
     comments = Column(Integer)
     points = Column(Integer)
     label = Column(String)
+
 
 Base.metadata.create_all(bind=engine)
