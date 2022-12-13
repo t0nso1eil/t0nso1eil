f4e0064ec0e40adc07a0a82dd89777e66a20d48f
t0nso1eil <73792532+t0nso1eil@users.noreply.github.com>
2022-12-13 18:21:42 +0300

    Update test bayes.py

--- homework06/test bayes.py
+++ homework06/test bayes.py
@@ -1,9 +1,6 @@
 from string import punctuation
-
 from sklearn.metrics import accuracy_score
-
 from bayes import NaiveBayesClassifier
-
 
 def read_sms():
     with open("data/SMSSpamCollection", "r", encoding="utf-8") as f:
